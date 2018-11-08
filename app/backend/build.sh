c4de6b7cbae37706b1bacbeb0fe2d7d5e603e6c6
Gonçalo Carvalho <goncalo.carvalho@quby.com>
2018-11-08 16:50:10 +0100

    added verification if git repo is dirty

--- app/backend/build.sh
+++ app/backend/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-VERSION=$(cat version.txt)
-NEWVERSION=$(./release_version.sh -p $VERSION)
-echo creating version $NEWVERSION
-docker build . --tag 0.0.0.0:5000/backend:$NEWVERSION
-echo $NEWVERSION > version.txt
-docker push 0.0.0.0:5000/backend:$NEWVERSION
+
+if [ -z "$(git status --porcelain)" ]; then 
+  VERSION=$(cat version.txt)
+  NEWVERSION=$(./release_version.sh -p $VERSION)
+  echo creating version $NEWVERSION
+  docker build . --tag 0.0.0.0:5000/backend:$NEWVERSION
+  echo $NEWVERSION > version.txt
+  docker push 0.0.0.0:5000/backend:$NEWVERSION
+else 
+  echo "Dirty Repo - Please commit your changes before build"
+fi
