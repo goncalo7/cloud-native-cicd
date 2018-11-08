f26edfa00e53ae643b1a08022c2a873783e5909f
Gonçalo Carvalho <goncalo.carvalho@quby.com>
2018-11-08 16:54:57 +0100

    tag git repo after create new version

--- app/backend/build.sh
+++ app/backend/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+NAME="$(dirname "$dir")"
 if [ -z "$(git status --porcelain)" ]; then 
   VERSION=$(cat version.txt)
   NEWVERSION=$(./release_version.sh -p $VERSION)
   echo creating version $NEWVERSION
-  docker build . --tag 0.0.0.0:5000/backend:$NEWVERSION
+  docker build . --tag 0.0.0.0:5000/$NAME:$NEWVERSION
+  git tag $NAME-$NEWVERSION
+  git push tags
   echo $NEWVERSION > version.txt
   docker push 0.0.0.0:5000/backend:$NEWVERSION
 else 
